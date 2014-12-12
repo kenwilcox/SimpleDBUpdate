925f9d30b509f37766a62bcea00656fd4c66bad1
Ken Wilcox <wilcoxk@gmail.com>
2014-12-12 11:32:02 -0700

    Create the SimpleDB object

--- SimpleDBUpdate/frmMain.cs
+++ SimpleDBUpdate/frmMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SimpleDBUpdate.Data;
 
 namespace SimpleDBUpdate
 {
@@ -14,6 +15,11 @@ namespace SimpleDBUpdate
     public frmMain()
     {
       InitializeComponent();
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+      SimpleDB db = new SimpleDB();
     }
   }
 }
